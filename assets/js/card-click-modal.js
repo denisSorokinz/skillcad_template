52d2d9ff829c42468f84a06b8bd57229ba5c2f34
denisSorokinz <d.sorokin@gmail.com>
2020-03-13 18:53:48 +0200

    MERGE FIX

--- assets/js/card-click-modal.js
+++ assets/js/card-click-modal.js
@@ -1,3 +1,4 @@
+// in case of hover(click) effect - only scale card
 function productCardClicked(cardNumber) {
     let currentCard = document.querySelector(`#product-card-${cardNumber}`);
     let currentCardHiddenSpan = currentCard.querySelector("span");
@@ -5,6 +6,7 @@ function productCardClicked(cardNumber) {
     currentCardHiddenSpan.classList.remove("long-description-none");
 }
 
+// in case of hover(click) effect - only scale card
 function productCardNotClicked(cardNumber) {
     let currentCard = document.querySelector(`#product-card-${cardNumber}`);
     let currentCardHiddenSpan = currentCard.querySelector("span");
@@ -13,19 +15,36 @@ function productCardNotClicked(cardNumber) {
 }
 
 
-let imageToInsert = document.createElement("img");
+
 let modalBody = document.querySelector(".modal-body");
 
 function openModalWithCurrentCard(cardNumber) {
-
-
-    fillModalWindow(cardNumber);
-}
-
-function fillModalWindow(cardNumber) {
     let currentCard = document.querySelector(`#product-card-${cardNumber}`);
+
+    let imageToInsertSrc = currentCard.dataset.image;
+    let titleToInsert = currentCard.dataset.title;
+    let firstLiToInsert = currentCard.querySelector(".card-content").innerHTML;
+    let secondLiToInsert = currentCard.dataset.secondli;
+    let thirdLiToInsert = currentCard.dataset.thirdli;
+    let fourthLiToInsert = currentCard.dataset.fourthli;
     //other dataToInsert
 
-    imageToInsert.src = `${currentCard.dataset.image}`;
-    modalBody.appendChild(imageToInsert);
+    let imgTag = `<img class="modal-product-img" src='${imageToInsertSrc}'>`;
+    let titleTag = `<p class="card-heading-modal">${titleToInsert}</p>`;
+    let firstLiTag = `<li class="card-li">${firstLiToInsert}</li>`;
+    let secondLiTag = `<li class="card-li">${secondLiToInsert}</li>`;
+    let thirdLiTag = `<li class="card-li">${thirdLiToInsert}</li>`;
+    let fourthLiTag = `<li class="card-li">${fourthLiToInsert}</li>`;
+
+    if (secondLiToInsert === undefined) {
+        secondLiTag = '';
+    }
+    if (thirdLiToInsert === undefined) {
+        thirdLiTag = '';
+    }
+    if (fourthLiToInsert === undefined) {
+        fourthLiTag = '';
+    }
+
+    modalBody.innerHTML = `${imgTag} ${titleTag} <ul class="card-ul">${firstLiTag} ${secondLiTag} ${thirdLiTag} ${fourthLiTag}</ul>`;
 }
